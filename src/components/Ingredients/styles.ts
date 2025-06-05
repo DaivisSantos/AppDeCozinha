3f3052519790ebe47377be10950609e7c747608d
DaivisSantos <daivissantos1@gmail.com>
2025-06-05 10:06:18 -0300

    Altualização no Style do Componente Ingredients

--- src/components/Ingredients/styles.ts
+++ src/components/Ingredients/styles.ts
@@ -4,6 +4,8 @@ export const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     flexWrap: 'wrap',
+    marginBottom: 12,
+    marginTop: 12,
     gap: 12,
   },
   ingredientsContent: {
